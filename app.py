4be3acc723a421a732d35bcf7c9cc462af238819
Eric Zhou <ericsuper@outlook.com>
2024-05-06 01:28:37 -0400

    Got story generation working properly but it's really slow

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
 import streamlit as st
 from dotenv import load_dotenv
-import pickle
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-# from langchain.memory import ConversationBufferMemory
-# from langchain.chains import ConversationalRetrievalChain
-from langchain_community.llms import HuggingFaceEndpoint
+# from langchain_community.llms import HuggingFaceEndpoint
+
+torch.set_default_device("cuda")
+
 
 authors = {
-    "Jerome David Salinger": "Author 1",
+    "Jerome David Salinger": "authentic dialogue, complex symbolism, descriptive setting, and character inner thoughts",
     "James Joyce": "Author 2",
     "Lucia Berlin": "Author 3",
 }
 
 
-def get_model():
-    return llm
+def process_input(input):
+    inputs = st.session_state.tokenizer(
+        input,
+        return_tensors="pt",
+        return_attention_mask=False,
+    )
+
+    outputs = st.session_state.model.generate(**inputs, max_length=300)
+    text = st.session_state.tokenizer.batch_decode(outputs)[0]
+    return text
 
 
 def main():
@@ -22,9 +32,13 @@ def main():
     st.set_page_config(page_title="Short Story Generator", page_icon=":books:")
     # st.write(css, unsafe_allow_html=True)
     if "model" not in st.session_state:
-        st.session_state.model = HuggingFaceEndpoint(
-            repo_id="google/flan-t5-xxl", temperature=0.5
+        st.session_state.model = AutoModelForCausalLM.from_pretrained(
+            "microsoft/phi-1_5", torch_dtype="auto"
         )
+
+    if "tokenizer" not in st.session_state:
+        st.session_state.tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-1_5")
+
     st.header("Generate Short Stories in your Favorite Author's Style!")
 
     # Create a form for user input
@@ -37,11 +51,12 @@ def main():
         submit_button = st.form_submit_button(label="Generate Story")
 
     if submit_button:
-        prompt = f"You are a professional creative writer focused on short stories. Write the beginning of a short story in the style of {author} on the following topics: {topic}"
-        print(prompt)
-        response = st.session_state.model.invoke(prompt)
-        print(response)
-        st.write(response)
+        prompt = f"Write a detailed story in the style of {author} on {topic}. Include a lot of {authors[author]}\n\nStory:\n"
+        with st.spinner("Generating story..."):
+            print(prompt)
+            response = process_input(prompt)
+            print(response)
+            st.write(response)
 
 
 if __name__ == "__main__":
